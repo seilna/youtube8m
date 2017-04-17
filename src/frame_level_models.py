6ebdcc92263a3bd57150c1bc79928f6fa31e5044
seil <seil.na@vision.snu.ac.kr>
2017-04-17 21:12:33 +0900

    Bidirectional LSTM model added.

--- src/frame_level_models.py
+++ src/frame_level_models.py
@@ -237,6 +237,58 @@ class LstmModel(models.BaseModel):
         vocab_size=vocab_size,
         **unused_params)
 
+class BiLstmModel(models.BaseModel):
+
+  def create_model(self, model_input, vocab_size, num_frames, **unused_params):
+    """Creates a model which uses a stack of LSTMs to represent the video.
+
+    Args:
+      model_input: A 'batch_size' x 'max_frames' x 'num_features' matrix of
+                   input features.
+      vocab_size: The number of classes in the dataset.
+      num_frames: A vector of length 'batch' which indicates the number of
+           frames for each video (before padding).
+
+    Returns:
+      A dictionary with a tensor containing the probability predictions of the
+      model in the 'predictions' key. The dimensions of the tensor are
+      'batch_size' x 'num_classes'.
+    """
+    lstm_size = FLAGS.lstm_cells
+    number_of_layers = FLAGS.lstm_layers
+
+    stacked_lstm_fw = tf.contrib.rnn.MultiRNNCell(
+            [
+                tf.contrib.rnn.BasicLSTMCell(
+                    lstm_size, forget_bias=1.0, state_is_tuple=False)
+                for _ in range(number_of_layers)
+                ], state_is_tuple=False)
+
+    stacked_lstm_bw = tf.contrib.rnn.MultiRNNCell(
+            [
+                tf.contrib.rnn.BasicLSTMCell(
+                    lstm_size, forget_bias=1.0, state_is_tuple=False)
+                for _ in range(number_of_layers)
+                ], state_is_tuple=False)
+
+
+    loss = 0.0
+
+    output, state = tf.nn.bidirectional_dynamic_rnn(
+			cell_fw=stacked_lstm_fw, 
+			cell_bw=stacked_lstm_bw,
+			inputs=model_input,
+			sequence_length=num_frames,
+			dtype=tf.float32)
+
+    state = tf.concat([state[0], state[1]], 1)
+    aggregated_model = getattr(video_level_models,
+                               FLAGS.video_level_classifier_model)
+
+    return aggregated_model().create_model(
+        model_input=state,
+        vocab_size=vocab_size,
+        **unused_params)
 class ordinalTopK(models.BaseModel):
   def create_model(self, model_input, vocab_size, num_frames, **unused_params):
     """Creates a model which uses a ordinal TopK to represent the video.

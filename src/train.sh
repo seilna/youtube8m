e44dda5b8f395e31def275e4aa42c32cc6f127f3
runhani <runhani@gmail.com>
2017-05-22 17:21:41 +0900

    train script added

--- src/train.sh
+++ src/train.sh
@@ -2,3 +2,6 @@
 
 MODEL_DIR=/tmp/yt8m
 python train.py --train_data_pattern='/path/to/features/train*.tfrecord' --model=LogisticModel --train_dir=$MODEL_DIR/video_level_logistic_model
+
+#train script
+#python train.py --train_data_pattern='/media/joyful/HDD/yt8m/video_train/train*.tfrecord' --model=Logistic_Multi_Layer_Model --train_dir=$MODEL_DIR/video_level_multi_layer_logistic_model --feature_names="mean_rgb, mean_audio" --feature_sizes="1024, 128" --label_loss="CenterLoss"
